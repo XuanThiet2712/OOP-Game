00ea2670b5703cd4133c2d287a138ba7f74b4214
Nguyen Thai Kha <nguyenthaikhahpt1@gmail.com>
2026-04-10 22:21:46 +0700

    update constructor

---
+++ Kiem.h
@@ -0,0 +1,25 @@
+#pragma once
+#include<iostream>
+#include<iomanip>
+#include<string>
+#include "VuKhi.h"
+
+using namespace std;
+class Kiem : public VuKhi {
+    private: 
+        float doBen; // độ bền (0 - 100)
+    public:
+        //Constructor
+        Kiem() : VuKhi() {
+            doBen == 100.0;
+        }
+        Kiem (string ten, int st, float td, float db) : VuKhi(ten,st, td) {
+            doBen = db;
+        }
+        //Hàm hủy
+        ~Kiem() {}
+        
+    
+}
+
+
